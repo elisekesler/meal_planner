a5c887bb81555e7c941b4ac62cec5e8ee95e9f0e
elisekesler <107507728+elisekesler@users.noreply.github.com>
2025-01-04 12:24:38 -0500

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,1 +1,9 @@
+# mysite/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),  # if you have 'myapp'
+]
 
